a61e04a6004aecbc6c4d8d64d4c0ef32803aa645
Hemen Solomon <hemensolomon83@gmail.com>
2026-05-07 16:15:30 +0300

    generated the maze using DFS algorithm

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -17,11 +18,63 @@ CELL_SIZE = 1
 northWall = [[1 for _ in range(COLS)] for _ in range(ROWS)]
 eastWall  = [[1 for _ in range(COLS)] for _ in range(ROWS)]
 
+visited = [[False for _ in range(COLS)] for _ in range(ROWS)]
+
 glMatrixMode(GL_PROJECTION)
 glLoadIdentity()
 glOrtho(0, COLS, 0, ROWS, -1, 1)
 
 glMatrixMode(GL_MODELVIEW)
+
+stack = []
+
+current_row = 0
+current_col = 0
+
+visited[current_row][current_col] = True
+
+def get_neighbors(r, c):
+    neighbors = []
+
+    if r > 0 and not visited[r - 1][c]:
+        neighbors.append((r - 1, c))  # up
+
+    if r < ROWS - 1 and not visited[r + 1][c]:
+        neighbors.append((r + 1, c))  # down
+
+    if c > 0 and not visited[r][c - 1]:
+        neighbors.append((r, c - 1))  # left
+
+    if c < COLS - 1 and not visited[r][c + 1]:
+        neighbors.append((r, c + 1))  # right
+
+    return neighbors
+
+def remove_walls(r1, c1, r2, c2):
+    if r1 == r2: # Horizontal movement
+        if c1 < c2: eastWall[r1][c1] = 0 # Moving East
+        else: eastWall[r2][c2] = 0       # Moving West
+    elif c1 == c2: # Vertical movement
+        if r1 < r2: northWall[r1][c1] = 0 # Moving North (Up)
+        else: northWall[r2][c2] = 0       # Moving South (Down)
+
+def generate_maze_step():
+    global current_row, current_col
+
+    neighbors = get_neighbors(current_row, current_col)
+
+    if neighbors:
+        next_cell = random.choice(neighbors)
+
+        stack.append((current_row, current_col))
+
+        remove_walls(current_row, current_col, next_cell[0], next_cell[1])
+
+        current_row, current_col = next_cell
+        visited[current_row][current_col] = True
+
+    elif stack:
+        current_row, current_col = stack.pop()
 
 def draw_maze():
     glColor3f(1, 1, 1)
@@ -65,7 +118,9 @@ while True:
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
+    generate_maze_step()
     draw_maze()
+    
 
     pygame.display.flip()
     pygame.time.wait(10)
